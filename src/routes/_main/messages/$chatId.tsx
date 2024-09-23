ffa02a0756ce9ccb9e4c15c6a2996a8a1f802423
Olamide <onabiyiolamide13@gmail.com>
2024-09-23 03:12:03 +0100

    Implemented messaging

--- src/routes/_main/messages/$chatId.tsx
+++ src/routes/_main/messages/$chatId.tsx
@@ -1,5 +1,5 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { IconSend3, IconChevronLeft } from "justd-icons";
+import { IconSend3, IconChevronLeft, IconLoader } from "justd-icons";
 import { Card } from "~ui/card";
 import { Heading } from "~ui/heading";
 import { Button, buttonStyles } from "~ui/button";
@@ -10,8 +10,9 @@ import {
 	chatMessagesQueryOptions,
 	getChatParticipantsQueryOptions,
 } from "~lib/query-options";
-import { useSuspenseQuery } from "@tanstack/react-query";
+import { useQueryClient, useSuspenseQuery } from "@tanstack/react-query";
 import { useEffect, useRef, useState } from "react";
+import { supabase } from "~lib/supabase";
 
 export const Route = createFileRoute("/_main/messages/$chatId")({
 	loader: ({ context: { queryClient, session }, params: { chatId } }) => {
@@ -31,6 +32,8 @@ export const Route = createFileRoute("/_main/messages/$chatId")({
 function MessagesChatId() {
 	const textareaRef = useRef<HTMLTextAreaElement>(null);
 	const [message, setMessage] = useState("");
+	const [isSending, setIsSending] = useState(false);
+	const queryClient = useQueryClient();
 	const { chatId } = Route.useParams();
 	const { userId } = Route.useLoaderData();
 	const { data: participants } = useSuspenseQuery(
@@ -51,10 +54,60 @@ function MessagesChatId() {
 		adjustHeight();
 	}, []);
 
+	useEffect(() => {
+		window.scrollTo({
+			top: document.body.scrollHeight,
+			behavior: "smooth",
+		});
+	}, []);
+
+	useEffect(() => {
+		const channel = supabase
+			.channel(`chat:${chatId}`)
+			.on(
+				"postgres_changes",
+				{
+					event: "INSERT",
+					schema: "public",
+					table: "messages",
+					filter: `chat_id=eq.${chatId}`,
+				},
+				(payload) => {
+					queryClient.invalidateQueries(chatMessagesQueryOptions(chatId));
+				},
+			)
+			.subscribe();
+
+		return () => {
+			supabase.removeChannel(channel);
+		};
+	}, [chatId, queryClient]);
+
+	const sendMessage = async () => {
+		setIsSending(true);
+		if (!message.trim()) return;
+
+		const { error } = await supabase.from("messages").insert({
+			content: message,
+			chat_id: chatId,
+			senderId: userId,
+		});
+
+		if (error) {
+			console.error("Error sending message:", error);
+		} else {
+			setMessage("");
+			adjustHeight();
+		}
+		setIsSending(false);
+	};
+
 	const handleInput = (value: string) => {
 		adjustHeight();
 		setMessage(value);
 	};
+
+	// TODO: Implement sending message
 
 	return (
 		<div className="flex flex-col h-full">
@@ -122,15 +175,17 @@ function MessagesChatId() {
 			<div className="flex gap-2 items-end">
 				<Textarea
 					className="flex-grow text-sm"
-					rows={1}
+					rows={2}
 					value={message}
 					onChange={handleInput}
 					placeholder="Type a message..."
 					isRequired
 					ref={textareaRef}
+					aria-label="Message Box"
+					isDisabled={isSending}
 				/>
-				<Button size="small" intent="primary">
-					<IconSend3 />
+				<Button size="small" intent="primary" onPress={sendMessage}>
+					{isSending ? <IconLoader className="animate-spin" /> : <IconSend3 />}
 					Send
 				</Button>
 			</div>
